544f704898cfc985715ab7c93c21baa859143993
John Williamson <johnjwilliamson@live.com>
2018-09-22 16:20:59 +0100

    encrypt + decrypt test

--- src/eosjs.js
+++ src/eosjs.js
@@ -1,16 +1,16 @@
 import Eos from "eosjs";
 
 export const seller = {
-    accountName: "dtradeseller",
-    pub: 'EOS5x2uxVezW7e4S5TQu9N7iFhZdFdzmkZzrSFcUt2xMqPcEVmQ2H',
-    priv: '5KCseM9bWMwmP8tL8C1MwcuVDGYHjwoNffBsD9gbAYhrBSrEqaU'
-}
+  accountName: "dtradeseller",
+  pub: "EOS5x2uxVezW7e4S5TQu9N7iFhZdFdzmkZzrSFcUt2xMqPcEVmQ2H",
+  priv: "5KCseM9bWMwmP8tL8C1MwcuVDGYHjwoNffBsD9gbAYhrBSrEqaU"
+};
 
 export const buyer = {
-    accountName: "dtradebuyer1",
-    pub: 'EOS77acvYqW1VZo6Zm4PynAdeiJMLmtgW8cxxUVPAsqg4i34GjYRC'
-    priv: '5JXzKFmdWPMqorB47T1YDfKkVrvcSJ8j31HSLYv3iwd3uFFQ393'
-}
+  accountName: "dtradebuyer1",
+  pub: "EOS77acvYqW1VZo6Zm4PynAdeiJMLmtgW8cxxUVPAsqg4i34GjYRC",
+  priv: "5JXzKFmdWPMqorB47T1YDfKkVrvcSJ8j31HSLYv3iwd3uFFQ393"
+};
 
 const config = {
   chainId: "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191", // 32 byte (64 char) hex string
@@ -22,4 +22,4 @@ const config = {
   sign: true
 };
 
-export default Eos(config);
+export const eos = Eos(config);
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import eos from "../eosjs";
+import { eos, buyer, seller } from "../eosjs";
 import Form from "../pagedraw/component_1";
+
+import { encrypt, decrypt } from "eos-communication-lib";
 
 class ProductDetail extends Component {
   constructor(props) {
@@ -21,7 +23,11 @@ class ProductDetail extends Component {
 
   async onBuy() {
     const { productid } = this.props.match.params;
-    
+    const message = JSON.stringify({...this.state})
+    const cipherText = encrypt(buyer.priv, seller.pub, message)
+    console.log(JSON.stringify(cipherText))
+    const decrypted = decrypt(seller.priv, buyer.pub, cipherText);
+    console.log(decrypted)
   }
 
   componentDidMount() {
