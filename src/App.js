3d36fc0b2706214eb6791baa6b1d9bdaad07cc5e
Denis Carriere <carriere.denis@gmail.com>
2018-09-22 22:17:20 +0100

    fix conflict

--- src/App.js
+++ src/App.js
@@ -19,53 +19,6 @@ import './App.css';
 import Dashboard from './pagedraw/component_1';
 import Two from './pagedraw/component_2';
 import ProductDetail from './pages/ProductDetail';
-<<<<<<< HEAD
-import * as EosWebSocket from "./eosws";
-import { Link, Route } from 'react-router-dom'
-
-const ws = new WebSocket("ws://35.203.114.193/v1/stream")
-
-ws.onopen = () => {
-  ws.send(EosWebSocket.get_actions("eosio.token", "transfer"));
-}
-
-ws.onmessage = (e) => {
-  if (e) {
-    const message = JSON.parse(e.data);
-
-    switch (message.type) {
-      case "ping":
-        console.log('ping');
-        break;
-      case "listening":
-        console.log('listening...');
-        break;
-      case "action_trace":
-        const data = message.data.trace.act.data
-        console.log(data)
-        break;
-      default:
-        console.log(message)
-    }
-  }
-}
-
-const App = () => (
-  <div>
-    <nav>
-      <Link to="/">Dashboard</Link>
-      <Link to={`/product/hello`}>Product</Link>
-    </nav>
-    <div>
-      {/*<Route path="/" component={Dashboard}/>*/}
-      <Route path="/product/:productid" component={ProductDetail} />
-      <Route path="/history/:accountname" component={Two} />
-    </div>
-  </div>
-)
-
-export default App;
-=======
 import RamTable from './pages/RamTable';
 
 import { Link, Route, Switch } from 'react-router-dom'
@@ -169,4 +122,3 @@ const App = props => {
 }
 
 export default withStyles(styles)(App);
->>>>>>> 0a79107f0cd8c404532d5f3384854fa28fbc160f
