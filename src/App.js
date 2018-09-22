274eb72e4bbbb51238bd898cc0f322e551b6c87a
Denis Carriere <carriere.denis@gmail.com>
2018-09-22 22:12:24 +0100

    Add eows

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import Dashboard from './pagedraw/component_1';
 import Two from './pagedraw/component_2';
 import ProductDetail from './pages/ProductDetail';
-
+import * as EosWebSocket from "./eosws";
 import { Link, Route } from 'react-router-dom'
+
+const ws = new WebSocket("ws://35.203.114.193/v1/stream")
+
+ws.onopen = () => {
+  ws.send(EosWebSocket.get_actions("eosio.token", "transfer"));
+}
+
+ws.onmessage = (e) => {
+  if (e) {
+    const message = JSON.parse(e.data);
+
+    switch (message.type) {
+      case "ping":
+        console.log('ping');
+        break;
+      case "listening":
+        console.log('listening...');
+        break;
+      case "action_trace":
+        const data = message.data.trace.act.data
+        console.log(data)
+        break;
+      default:
+        console.log(message)
+    }
+  }
+}
 
 const App = () => (
   <div>
