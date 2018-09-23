7d2d002b74d474387948f0dc3e46635e7a6ae3ad
John Williamson <johnjwilliamson@live.com>
2018-09-23 08:18:19 +0100

    added name

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -6,14 +6,13 @@ import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
 import CardMedia from "@material-ui/core/CardMedia";
 
-
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
-import { Route } from 'react-router-dom'
+import { Route } from "react-router-dom";
 
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 
 import "./ProductDetail.css";
 
@@ -59,9 +58,9 @@ class ProductDetail extends Component {
   }
 
   async onBuy(id) {
-      console.log(buyer)
+    console.log(buyer);
     const { productid } = this.props.match.params;
-    this.setState({ processing: true })
+    this.setState({ processing: true });
     console.log(productid, "you have");
     const message = JSON.stringify({ ...this.state });
     console.log(message.length);
@@ -93,8 +92,8 @@ class ProductDetail extends Component {
         }
       ]
     });
-    this.setState({ processing: false })
-    this.props.history.push("/orders")
+    this.setState({ processing: false });
+    this.props.history.push("/orders");
     console.log(result);
   }
 
@@ -106,48 +105,51 @@ class ProductDetail extends Component {
   }
 
   render() {
-
     return !this.state.product ? (
       <h1>Loading...</h1>
     ) : (
       <div className="container">
         <div className="topper">
-        <Grid container spacing={24}>
-         <Grid item xs={6}>
-          <div className="imageContainer">
-            <img src={"https://www.afrofood.com/wp-content/uploads/2012/08/hand-woven-african-basket.jpg"} />
-          </div>
-          </Grid>
-          <Grid item xs={6}>
-          <div className="x">
-            <List>
-              <ListItem>
-                <ListItemText
-                  primary="Seller Reputation"
-                  secondary="89"
+          <Grid container spacing={24}>
+            <Grid item xs={6}>
+              <div className="imageContainer">
+                <img
+                  src={
+                    "https://www.afrofood.com/wp-content/uploads/2012/08/hand-woven-african-basket.jpg"
+                  }
                 />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary="Escrow"
-                  secondary={this.state.product.escrow}
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary="Seller"
-                  secondary={this.state.product.seller}
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary="Price"
-                  secondary={this.state.product.price}
-                />
-              </ListItem>
-            </List>
-          </div>
-          </Grid>
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <div className="x">
+                <List>
+                  <ListItem>
+                    <ListItemText primary="About" secondary="Genuine Handwoven Basket" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText primary="Seller Reputation" secondary="89" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary="Escrow"
+                      secondary={this.state.product.escrow}
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary="Seller"
+                      secondary={this.state.product.seller}
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary="Price"
+                      secondary={this.state.product.price}
+                    />
+                  </ListItem>
+                </List>
+              </div>
+            </Grid>
           </Grid>
         </div>
         <Divider />
@@ -163,7 +165,7 @@ class ProductDetail extends Component {
           size="large"
           color="primary"
         >
-          {this.state.processing? 'Processing...' : 'Buy'}
+          {this.state.processing ? "Processing..." : "Buy"}
         </Button>
       </div>
     );
